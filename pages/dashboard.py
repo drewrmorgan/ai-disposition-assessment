5087b45c4d8abaad0bfbe1faff70dce94c0d5c58
drewrmorgan <drewrmorgan@gmail.com>
2026-07-02 15:16:41 -0500

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,16 +1,37 @@
 import streamlit as st
 import pandas as pd
 
+# ----------------------------------
+# PASSWORD PROTECTION
+# ----------------------------------
+
+password = st.text_input(
+    "Enter Dashboard Password",
+    type="password"
+)
+
+if password != st.secrets["dashboard_password"]:
+    st.warning(
+        "Enter the dashboard password to access dashboard data."
+    )
+    st.stop()
+
+# ----------------------------------
+# DASHBOARD
+# ----------------------------------
+
 st.title("AI Dispositions Dashboard")
 
 try:
 
     df = pd.read_excel(
-    "AI_Disposition_Responses.xlsx",
-    engine="openpyxl"
-)
+        "AI_Disposition_Responses.xlsx",
+        engine="openpyxl"
+    )
 
-    st.write(df)
+    st.success(f"Total Responses: {len(df)}")
+
+    st.dataframe(df)
 
 except FileNotFoundError:
 
